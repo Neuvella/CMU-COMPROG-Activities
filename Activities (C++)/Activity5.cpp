54064f0b2173601e49f94a31e0c9b7b7e59746b2
Neuvella <117031888+Neuvella@users.noreply.github.com>
2026-02-07 21:48:42 +0800

    Add files via upload

---
+++ Activities (C++)/Activity5.cpp
@@ -0,0 +1,22 @@
+// Activity 5
+// Find the area and perimeter of a square
+
+#include <iostream>
+using namespace std;
+int main() {
+
+    float length, width;
+    int area, perimeter;
+
+    cout << "What is the length of the square?: ";
+    cin >> length;
+    cout << "What is the width of the square?: ";
+    cin >> width;
+
+    area = length * length;
+    perimeter = width * 4;
+
+    cout << "The area of the square is " << area << " and the perimeter is " << perimeter << "." << endl;
+
+    return 0;
+}
