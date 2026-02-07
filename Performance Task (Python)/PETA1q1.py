592e45d50752c337e766fbcd8973ead74ad83e5a
Neuvella <117031888+Neuvella@users.noreply.github.com>
2026-02-07 22:59:35 +0800

    Add even/odd checker based on last binary bit
    Implement a program to determine if a number is even or odd based on its last binary bit.

---
+++ Performance Task (Python)/PETA1q1.py
@@ -0,0 +1,9 @@
+# show how the last binary bit determines if a number is even or odd
+
+num = int(input("Enter the last number of the binary of the number you want")) 
+if(num == 1): 
+    print("The number is odd") 
+elif(num == 0): 
+    print("The number is even")     
+else: 
+    print("Invalid number") 
